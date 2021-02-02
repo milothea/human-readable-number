fe05cf8a805c67e651dbda7cf8326b2885089d0d
thea <thea@MacBook-Pro-Lulu.local>
2021-02-02 22:36:12 +0300

    feat: add add implementation of convertation from number to readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+    const   numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+            dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+            numberStr = number.toString(),
+            lengthStr = numberStr.length;
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (lengthStr === 1) {
+        return numbers[number - 1];
+    }
+
+    if (lengthStr === 2 && number < 20) {
+        return numbers[number - 1];
+    } else if (lengthStr === 2 && number % 10 === 0) {
+        return dozens[numberStr[0] - 1];
+    } else if (lengthStr === 2) {
+        return `${dozens[+numberStr[0] - 1]} ${numbers[+numberStr[1] - 1]}`;
+    }
+
+    if (lengthStr  === 3 && +(numberStr.slice(1)) === 0) {
+        return `${numbers[+numberStr[0] - 1]} hundred`;
+    } else {
+        return `${numbers[+numberStr[0] - 1]} hundred ${toReadable(+(numberStr.slice(1)))}`;
+    }
 }
